49c4f2672737b26a22578cac89f51bd478ac4acf
Yash Shah <47370029+yshah2232@users.noreply.github.com>
2019-02-08 13:01:53 -0500

    Update MVA1.R

--- MVA1.R
+++ MVA1.R
@@ -1,4 +1,2 @@
-setwd("C:/Users/Yshah/Downloads/Rutgers Sem 2/MVA")
-wisc_bc_df <- read.csv("./wisc_bc_data.csv", stringsAsFactors = F)
-str(wisc_bc_df)
+wisc_bc_df <- read.csv("C:/Users/Yshah/Downloads/Rutgers Sem 2/MVA")
 wisc_bc_df
