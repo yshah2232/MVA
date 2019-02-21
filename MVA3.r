17a3bb1e59096604be025dbbae155007fdb8be08
Yash Shah <47370029+yshah2232@users.noreply.github.com>
2019-02-21 18:34:37 -0500

    Add files via upload

--- MVA3.r
+++ MVA3.r
@@ -15,7 +15,7 @@ summary(cancer)
 #Gives you frequency table
 diagnosis.table <- table(cancer$diagnosis)
 diagnosis.table
- 
+
 
 #Bar plot
 ggplot(data=cancer, aes(x=diagnosis)) + geom_bar(stat = "count") 
@@ -60,4 +60,69 @@ corrplot(corMatMy, order = "hclust", tl.cex = 0.7)
 #Scatterplot Matrix
 pairs(~radius_mean+perimeter_mean+area_mean+compactness_mean+concavity_mean,data = cancer,main = "Scatterplot Matrix",col=c("blue","green","yellow","red"))
 
+names(cancer)
+#Multivariate analysis
+#T TEST
+with(data=cancer,t.test(radius_mean[diagnosis=="B"],radius_mean[diagnosis=="M"],var.equal=TRUE))
+with(data=cancer,t.test(texture_mean[diagnosis=="B"],texture_mean[diagnosis=="M"],var.equal=TRUE))
+with(data=cancer,t.test(perimeter_mean[diagnosis=="B"],perimeter_mean[diagnosis=="M"],var.equal=TRUE))
+with(data=cancer,t.test(area_mean[diagnosis=="B"],area_mean[diagnosis=="M"],var.equal=TRUE))
+with(data=cancer,t.test(smoothness_mean[diagnosis=="B"],smoothness_mean[diagnosis=="M"],var.equal=TRUE))
+with(data=cancer,t.test(compactness_mean[diagnosis=="B"],compactness_mean[diagnosis=="M"],var.equal=TRUE))
+with(data=cancer,t.test(concavity_mean[diagnosis=="B"],concavity_mean[diagnosis=="M"],var.equal=TRUE))
+with(data=cancer,t.test(points_mean[diagnosis=="B"],points_mean[diagnosis=="M"],var.equal=TRUE))
+with(data=cancer,t.test(symmetry_mean[diagnosis=="B"],symmetry_mean[diagnosis=="M"],var.equal=TRUE))
+with(data=cancer,t.test(dimension_mean[diagnosis=="B"],dimension_mean[diagnosis=="M"],var.equal=TRUE))
 
+#Hotelling's T2 test
+#install.packages("Hotelling")
+library(Hotelling)
+t2testcan <- hotelling.test(radius_mean + texture_mean + perimeter_mean + area_mean + smoothness_mean + compactness_mean + concavity_mean + points_mean + symmetry_mean + dimension_mean ~ diagnosis, data=cancer)
+# Output of the function hotelling.test is given
+cat("T2 statistic =",t2testcan$stat[[1]],"\n")
+#print(t2testcan)
+#  T2 statistic is located in the first element of the list "stat"
+#View(t2testcan)
+
+
+
+#Levene Test
+matstand <- scale(cancer[,3:10])
+matstand
+matsurv <- matstand[cancer$diagnosis =="B",]
+matsurv
+matnosurv <- matstand[cancer$diagnosis == "M",]
+vecmediansurv <- apply(matsurv, 2, median)
+# in the above 2 represents column. Hence, we are asking for column median
+vecmediansurv
+
+vecmediannosurv <- apply(matnosurv, 2, median)
+matabsdevsurv <- abs(matsurv - matrix(rep(vecmediansurv,nrow(matsurv)),nrow=nrow(matsurv), byrow=TRUE))
+
+matabsdevnosurv <- abs(matnosurv - matrix(rep(vecmediannosurv,nrow(matnosurv)),nrow=nrow(matnosurv), byrow=TRUE))
+
+matabsdevnosurv
+
+matabsdev.all <- rbind(matabsdevsurv,matabsdevnosurv)
+matabsdev.all <- data.frame(cancer$diagnosis, matabsdev.all)
+
+t.test(matabsdev.all$radius_mean[cancer$diagnosis == "B"],matabsdev.all$radius_mean[cancer$diagnosis == "M"], alternative="less",var.equal = TRUE)
+t.test(matabsdev.all$texture_mean[cancer$diagnosis == "B"],matabsdev.all$texture_mean[cancer$diagnosis == "M"], alternative="less",var.equal = TRUE)
+t.test(matabsdev.all$perimeter_mean[cancer$diagnosis == "B"],matabsdev.all$perimeter_mean[cancer$diagnosis == "M"], alternative="less",var.equal = TRUE)
+t.test(matabsdev.all$area_mean[cancer$diagnosis == "B"],matabsdev.all$area_mean[cancer$diagnosis == "M"], alternative="less",var.equal = TRUE)
+t.test(matabsdev.all$smoothness_mean[cancer$diagnosis == "B"],matabsdev.all$smoothness_mean[cancer$diagnosis == "M"], alternative="less",var.equal = TRUE)
+t.test(matabsdev.all$compactness_mean[cancer$diagnosis == "B"],matabsdev.all$compactness_mean[cancer$diagnosis == "M"], alternative="less",var.equal = TRUE)
+t.test(matabsdev.all$concavity_mean[cancer$diagnosis == "B"],matabsdev.all$concavity_mean[cancer$diagnosis == "M"], alternative="less",var.equal = TRUE)
+t.test(matabsdev.all$points_mean[cancer$diagnosis == "B"],matabsdev.all$points_mean[cancer$diagnosis == "M"], alternative="less",var.equal = TRUE)
+
+
+matstand
+matstand.all <- data.frame(cancer$diagnosis, matstand)
+matstand.all
+colnames(matstand.all) <- colnames(cancer[2:10])
+t2testcan <- hotelling.test(radius_mean + texture_mean + perimeter_mean + area_mean + smoothness_mean + compactness_mean + concavity_mean + points_mean + symmetry_mean + dimension_mean ~ diagnosis, data=cancer)
+cat("T2 statistic =",t2testcan$stat[[1]],"\n")
+print(t2testcan)
+
+# In the above we standardized using scale function
+matabsdev.all
