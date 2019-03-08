78cf750cff0c26f1c8bfd72d52b0f03fb2b82c80
Yash Shah <47370029+yshah2232@users.noreply.github.com>
2019-03-08 16:58:24 -0500

    Add files via upload

--- MVA3.r
+++ MVA3.r
@@ -6,6 +6,8 @@ library("ggplot2")
 library("corrplot")
 library("reshape")
 
+
+
 #number of obsevations and data type
 str(cancer)
 
@@ -141,3 +143,155 @@ leveneTest(concavity_mean~ diagnosis, data=cancer)
 leveneTest(points_mean ~ diagnosis, data=cancer)
 leveneTest(symmetry_mean ~ diagnosis, data=cancer)
 leveneTest(dimension_mean ~ diagnosis, data=cancer)
+
+
+#PCA Analysis
+#PCA
+
+dim(cancer)
+attach(cancer)
+head(cancer)
+#Get the Correlations between the measurements
+cor(cancer[-2])
+c <- (cor(cancer[-2]))
+plot(c)
+# Using prcomp to compute the principal components (eigenvalues and eigenvectors). With scale=TRUE, variable means are set to zero, and variances set to one
+cancer_pca <- prcomp(cancer[,-2],scale=TRUE)
+cancer_pca
+plot(cancer_pca)
+summary(cancer_pca)
+#View(cancer_pca)
+head(cancer_pca$x)
+# sample scores stored in cancer_pca$x
+# singular values (square roots of eigenvalues) stored in cancer_pca$sdev
+# loadings (eigenvectors) are stored in cancer_pca$rotation
+# variable means stored in cancer_pca$center
+# variable standard deviations stored in sparrows_pca$scale
+# A table containing eigenvalues and %'s accounted, follows
+# Eigenvalues are sdev^2
+(eigen_cancer <- cancer_pca$sdev^2) ## brackets for print
+names(eigen_cancer) <- paste("PC",1:31,sep="")
+eigen_cancer
+sumlambdas <- sum(eigen_cancer)
+sumlambdas
+propvar <- eigen_cancer/sumlambdas
+propvar
+summary(eigen_cancer)
+summary(cancer_pca)
+cumvar_cancer <- cumsum(propvar)
+cumvar_cancer
+matlambdas <- rbind(eigen_cancer,propvar,cumvar_cancer)
+rownames(matlambdas) <- c("Eigenvalues","Prop. variance","Cum. prop. variance")
+round(matlambdas,4)
+summary(cancer_pca)
+cancer_pca$rotation
+print(cancer_pca)
+# Sample scores stored in cancer_pca$x
+cancer_pca$x
+# Identifying the scores by their diagnosis
+diag_pca <- cbind(data.frame(diagnosis),cancer_pca$x)
+diag_pca
+# Means of scores for all the PC's classified by diagnosis status
+tabmeansPC <- aggregate(diag_pca[,2:31],by=list(diagnosis=cancer$diagnosis),mean)
+tabmeansPC
+tabmeansPC <- tabmeansPC[rev(order(tabmeansPC$diagnosis)),]
+tabmeansPC
+tabfmeans <- t(tabmeansPC[,-1])
+tabfmeans
+colnames(tabfmeans) <- t(as.vector(tabmeansPC[1]))
+tabfmeans
+# Standard deviations of scores for all the PC's classified by diagnosis status
+tabsdsPC <- aggregate(diag_pca[,2:31],by=list(cancer$diagnosis),sd)
+tabfsds <- t(tabsdsPC[,-1])
+colnames(tabfsds) <- t(as.vector(tabsdsPC[1]))
+tabfsds
+t.test(PC1~cancer$diagnosis,data=diag_pca)
+t.test(PC2~cancer$diagnosis,data=diag_pca)
+t.test(PC3~cancer$diagnosis,data=diag_pca)
+t.test(PC4~cancer$diagnosis,data=diag_pca)
+t.test(PC5~cancer$diagnosis,data=diag_pca)
+t.test(PC6~cancer$diagnosis,data=diag_pca)
+t.test(PC7~cancer$diagnosis,data=diag_pca)
+t.test(PC8~cancer$diagnosis,data=diag_pca)
+t.test(PC9~cancer$diagnosis,data=diag_pca)
+t.test(PC10~cancer$diagnosis,data=diag_pca)
+t.test(PC11~cancer$diagnosis,data=diag_pca)
+t.test(PC12~cancer$diagnosis,data=diag_pca)
+t.test(PC13~cancer$diagnosis,data=diag_pca)
+t.test(PC14~cancer$diagnosis,data=diag_pca)
+t.test(PC15~cancer$diagnosis,data=diag_pca)
+t.test(PC16~cancer$diagnosis,data=diag_pca)
+t.test(PC17~cancer$diagnosis,data=diag_pca)
+t.test(PC18~cancer$diagnosis,data=diag_pca)
+t.test(PC19~cancer$diagnosis,data=diag_pca)
+t.test(PC20~cancer$diagnosis,data=diag_pca)
+t.test(PC21~cancer$diagnosis,data=diag_pca)
+t.test(PC22~cancer$diagnosis,data=diag_pca)
+t.test(PC23~cancer$diagnosis,data=diag_pca)
+t.test(PC24~cancer$diagnosis,data=diag_pca)
+t.test(PC25~cancer$diagnosis,data=diag_pca)
+t.test(PC26~cancer$diagnosis,data=diag_pca)
+t.test(PC27~cancer$diagnosis,data=diag_pca)
+t.test(PC28~cancer$diagnosis,data=diag_pca)
+t.test(PC29~cancer$diagnosis,data=diag_pca)
+t.test(PC30~cancer$diagnosis,data=diag_pca)
+t.test(PC31~cancer$diagnosis,data=diag_pca)
+
+# F ratio tests
+var.test(PC1~cancer$diagnosis,data=diag_pca)
+var.test(PC2~cancer$diagnosis,data=diag_pca)
+var.test(PC3~cancer$diagnosis,data=diag_pca)
+var.test(PC4~cancer$diagnosis,data=diag_pca)
+var.test(PC5~cancer$diagnosis,data=diag_pca)
+var.test(PC6~cancer$diagnosis,data=diag_pca)
+var.test(PC7~cancer$diagnosis,data=diag_pca)
+var.test(PC8~cancer$diagnosis,data=diag_pca)
+var.test(PC9~cancer$diagnosis,data=diag_pca)
+var.test(PC10~cancer$diagnosis,data=diag_pca)
+var.test(PC11~cancer$diagnosis,data=diag_pca)
+var.test(PC12~cancer$diagnosis,data=diag_pca)
+var.test(PC13~cancer$diagnosis,data=diag_pca)
+var.test(PC14~cancer$diagnosis,data=diag_pca)
+var.test(PC15~cancer$diagnosis,data=diag_pca)
+var.test(PC16~cancer$diagnosis,data=diag_pca)
+var.test(PC17~cancer$diagnosis,data=diag_pca)
+var.test(PC18~cancer$diagnosis,data=diag_pca)
+var.test(PC19~cancer$diagnosis,data=diag_pca)
+var.test(PC20~cancer$diagnosis,data=diag_pca)
+var.test(PC21~cancer$diagnosis,data=diag_pca)
+var.test(PC22~cancer$diagnosis,data=diag_pca)
+var.test(PC23~cancer$diagnosis,data=diag_pca)
+var.test(PC24~cancer$diagnosis,data=diag_pca)
+var.test(PC25~cancer$diagnosis,data=diag_pca)
+var.test(PC26~cancer$diagnosis,data=diag_pca)
+var.test(PC27~cancer$diagnosis,data=diag_pca)
+var.test(PC28~cancer$diagnosis,data=diag_pca)
+var.test(PC29~cancer$diagnosis,data=diag_pca)
+var.test(PC30~cancer$diagnosis,data=diag_pca)
+var.test(PC31~cancer$diagnosis,data=diag_pca)
+# Levene's tests (one-sided)
+library(car)
+(LTPC1 <- leveneTest(PC1~cancer$diagnosis,data=diag_pca))
+(p_PC1_1sided <- LTPC1[[3]][1]/2)
+(LTPC2 <- leveneTest(PC2~cancer$diagnosis,data=diag_pca))
+(p_PC2_1sided=LTPC2[[3]][1]/2)
+(LTPC3 <- leveneTest(PC3~cancer$diagnosis,data=diag_pca))
+(p_PC3_1sided <- LTPC3[[3]][1]/2)
+(LTPC4 <- leveneTest(PC4~cancer$diagnosis,data=diag_pca))
+(p_PC4_1sided <- LTPC4[[3]][1]/2)
+(LTPC5 <- leveneTest(PC5~cancer$diagnosis,data=diag_pca))
+(p_PC5_1sided <- LTPC5[[3]][1]/2)
+# Plotting the scores for the first and second components
+plot(diag_pca$PC1, diag_pca$PC2,pch=ifelse(diag_pca$diagnosis == "S",1,16),xlab="PC1", ylab="PC2", main="569 entries against values for PC1 & PC2")
+abline(h=0)
+abline(v=0)
+legend("bottomleft", legend=c("Benign","Malignant"), pch=c(1,16))
+plot(eigen_cancer, xlab = "Component number", ylab = "Component variance", type = "l", main = "Scree diagram")
+plot(log(eigen_cancer), xlab = "Component number",ylab = "log(Component variance)", type="l",main = "Log(eigenvalue) diagram")
+print(summary(cancer_pca))
+#View(cancer_pca)
+diag(cov(cancer_pca$x))
+xlim <- range(cancer_pca$x[,1])
+cancer_pca$x[,1]
+cancer_pca$x
+plot(cancer_pca$x,xlim=xlim,ylim=xlim)
